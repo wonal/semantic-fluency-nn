ad1bd731a96d8350081ac19abf2e9ddf4b05c1ee
Jacob Collins <jacob.m.collins2@gmail.com>
2019-03-05 20:02:53 -0800

    Modified update functionality in semantic network, added more tests

--- src/graph/semantic_network.py
+++ src/graph/semantic_network.py
@@ -15,51 +15,19 @@ class SemanticNetwork:
         self._init_maps()
         self._init_graph()
 
-    def update(self, em_proportion=0.1, g_proportion=0.1, include_set=None, stop_set=None, thresh=0.7, verbose=False):
-        if not include_set and not stop_set:
-            em_subset = np.array(
-                [idx for idx in _select_subset(
-                    self.embedding_matrix, self.embedding_matrix.shape[0], subset_proportion=em_proportion
-                ) if self.index_to_key[idx]]
-            )
-            g_subset = np.array(
-                [idx for idx in _select_subset(
-                    self.graph.adjacency_matrix, len(self.graph.nodes), subset_proportion=g_proportion
-                ) if self.index_to_key[idx]]
-            )
-        elif include_set and not stop_set:
-            em_subset = np.array(
-                [idx for idx in _select_subset(
-                    self.embedding_matrix, self.embedding_matrix.shape[0], subset_proportion=em_proportion
-                ) if self.index_to_key[idx] in include_set]
-            )
-            g_subset = np.array(
-                [idx for idx in _select_subset(
-                    self.graph.adjacency_matrix, len(self.graph.nodes), subset_proportion=g_proportion
-                ) if self.index_to_key[idx] in include_set]
-            )
-        elif not include_set and stop_set:
-            em_subset = np.array(
-                [idx for idx in _select_subset(
-                    self.embedding_matrix, self.embedding_matrix.shape[0], subset_proportion=em_proportion
-                ) if self.index_to_key[idx] not in stop_set]
-            )
-            g_subset = np.array(
-                [idx for idx in _select_subset(
-                    self.graph.adjacency_matrix, len(self.graph.nodes), subset_proportion=g_proportion
-                ) if self.index_to_key[idx] not in stop_set]
-            )
-        else:
-            em_subset = np.array(
-                [idx for idx in _select_subset(
-                    self.embedding_matrix, self.embedding_matrix.shape[0], subset_proportion=em_proportion
-                ) if self.index_to_key[idx] in include_set and self.index_to_key[idx] not in stop_set]
-            )
-            g_subset = np.array(
-                [idx for idx in _select_subset(
-                    self.graph.adjacency_matrix, len(self.graph.nodes), subset_proportion=g_proportion
-                ) if self.index_to_key[idx] in include_set and self.index_to_key[idx] not in stop_set]
-            )
+    def update(self, em_proportion=0.1, g_proportion=0.1, include_set=None, stop_set=set(), thresh=0.7, verbose=False):
+        if include_set is None: include_set = self.graph.nodes
+
+        em_subset = np.array(
+            [idx for idx in _select_subset(
+                self.embedding_matrix, self.embedding_matrix.shape[0], subset_proportion=em_proportion
+            ) if self.index_to_key[idx] in include_set if self.index_to_key[idx] not in stop_set]
+        )
+        g_subset = np.array(
+            [idx for idx in _select_subset(
+                self.graph.adjacency_matrix, len(self.graph.nodes), subset_proportion=g_proportion
+            ) if self.index_to_key[idx] in include_set if self.index_to_key[idx] not in stop_set]
+        )
 
         self._update(em_subset, g_subset, thresh, verbose)
 
