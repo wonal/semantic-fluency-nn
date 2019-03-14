41b4c68a49a99d70f1058620847c41c67be2ff03
Alli Wong <wonga10@gmail.com>
2019-03-14 16:32:18 -0700

    added methods for custom, highest-degree, and most-similar word plots

--- src/visualization/relational_graph.py
+++ src/visualization/relational_graph.py
@@ -12,19 +12,11 @@ class RelationalGraph:
 
     def create_relational_graph(self, n):
         from_nodes, to_nodes = self._retrieve_n_largest(n)
-
         from_nodes = np.apply_along_axis(self._retrieve_word, 0, from_nodes)
         to_nodes = np.apply_along_axis(self._retrieve_word, 0, to_nodes)
         self._plot_nodes(from_nodes, to_nodes)
-
-
-
-
-    def _retrieve_word(self, index):
-        words = []
-        for i in index:
-            words.append(self.network.index_to_key[i])
-        return np.array(words)
+        #from_list, to_list = self.create_relational_lists(from_nodes, to_nodes)
+        #self._plot_nodes(from_list, to_list)
 
     def _retrieve_n_largest(self, n):
         x_dim, y_dim = self.adj_matrix.shape
@@ -33,10 +25,43 @@ class RelationalGraph:
         coordinates = np.unravel_index(top_indices, (x_dim, y_dim))
         return coordinates
 
+    def _retrieve_word(self, index):
+        words = []
+        for i in index:
+            words.append(self.network.index_to_key[i])
+        return np.array(words)
+
     @staticmethod
     def _plot_nodes(from_list, to_list):
         df = pd.DataFrame({'from': from_list, 'to': to_list})
         graph = nx.from_pandas_edgelist(df, 'from', 'to')
         nx.draw(graph, with_labels=True, node_size=1000, node_color="orange", edge_color='black', alpha=0.5, width=2)
         plt.show()
+
+    def create_most_connected_graph(self, n):
+        most_connected = self.retrieve_most_connected(n)
+        from_list, to_list = self.create_relational_lists(most_connected, np.array([]))
+        self._plot_nodes(from_list, to_list)
+
+    def retrieve_most_connected(self, n):
+        row_totals = np.sum(self.adj_matrix, axis=0)
+        indices = np.argsort(row_totals)[-n:]
+        nodes = np.take(self.network.graph.nodes, indices)
+        return nodes
+
+    def create_relational_lists(self, from_nodes, to_nodes):
+        from_list = np.array([])
+        to_list = to_nodes
+        for node in from_nodes:
+            neighbors = self.network.graph.expand(node)
+            new_connections = np.repeat(node, len(neighbors))
+            from_list = np.concatenate((from_list, new_connections))
+            neighbor_words = np.array([x[0] for x in list(neighbors)])
+            to_list = np.concatenate((neighbor_words, to_list))
+        return from_list, to_list
+
+    def create_custom_relational(self, words):
+        from_list, to_list = self.create_relational_lists(words, np.array([]))
+        self._plot_nodes(from_list, to_list)
+
 
