46d83767d18901376fc0254e2cdc2c4e28b6cdba
Alli Wong <wonga10@gmail.com>
2019-03-14 07:54:55 -0700

    fixed constructor and break condition

--- src/algorithms/simulated_annealing.py
+++ src/algorithms/simulated_annealing.py
@@ -10,9 +10,7 @@ class SimulatedAnnealer:
     def __init__(self, graph, initial_temp=1000, start=None):
         if initial_temp < 1:
             raise ValueError('Temperature must be greater than zero.')
-        if start:
-            self.start = start
-        self.start = None
+        self.start = start
         self._graph = graph
         self._temperature = initial_temp
         self._initial_t = initial_temp
@@ -37,7 +35,7 @@ class SimulatedAnnealer:
         path = [current_state[0]]
         for i in range(1, n):
             neighbors = self._graph.expand(current_state[0])
-            if not neighbors and self._temperature <= 0:
+            if not neighbors or self._temperature <= 0:
                 break
             next_state = neighbors[np.random.choice(len(neighbors))]
             delta_state = next_state[1] - current_state[1]
