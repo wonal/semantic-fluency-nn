32e7f228b696bf66f8aea3f0c991faac09af1999
Alli Wong <wonga10@gmail.com>
2019-03-15 10:58:16 -0700

    added colored path plot

--- src/visualization/relational_graph.py
+++ src/visualization/relational_graph.py
@@ -10,12 +10,12 @@ class RelationalGraph:
         self.adj_matrix = network.graph.adjacency_matrix
         self.network = network
 
-    def create_relational_graph(self, n):
+    def create_most_similar_pairs_graph(self, n):
         from_nodes, to_nodes = self._retrieve_n_largest(n)
         from_nodes = np.apply_along_axis(self._retrieve_word, 0, from_nodes)
         to_nodes = np.apply_along_axis(self._retrieve_word, 0, to_nodes)
-        self._plot_nodes(from_nodes, to_nodes)
-        #from_list, to_list = self.create_relational_lists(from_nodes, to_nodes, True)
+        self._plot_nodes(from_nodes, to_nodes, [])
+        #from_list, to_list = self.create_relational_lists(from_nodes, to_nodes)
         #self._plot_nodes(from_list, to_list)
 
     def _retrieve_n_largest(self, n):
@@ -32,30 +32,42 @@ class RelationalGraph:
         return np.array(words)
 
     @staticmethod
-    def _plot_nodes(from_list, to_list):
+    def _plot_nodes(from_list, to_list, with_color_map):
         df = pd.DataFrame({'from': from_list, 'to': to_list})
         graph = nx.from_pandas_edgelist(df, 'from', 'to')
-        nx.draw(graph, with_labels=True, node_size=1000, node_color="orange", edge_color='black', alpha=0.6, width=1, font_weight='bold')
+        color_map = []
+        if with_color_map:
+            for node in graph.nodes:
+                if node in with_color_map:
+                    color_map.append('green')
+                else:
+                    color_map.append('orange')
+        else:
+            color_map = ['orange']
+        nx.draw(graph, with_labels=True, node_size=1000, node_color=color_map, edge_color='black', alpha=0.6, width=1, font_weight='bold')
         plt.show()
 
     def create_most_connected_graph(self, n):
-        most_connected = self.retrieve_most_connected(n)
-        from_list, to_list = self.create_relational_lists(most_connected, np.array([]), True)
-        self._plot_nodes(from_list, to_list)
+        most_connected = self._retrieve_most_connected(n)
+        from_list, to_list = self._create_relational_lists(most_connected, np.array([]))
+        self._plot_nodes(from_list, to_list, [])
 
-    def retrieve_most_connected(self, n):
+    def _retrieve_most_connected(self, n):
         row_totals = np.sum(self.adj_matrix, axis=0)
         indices = np.argsort(row_totals)[-n:]
         nodes = np.take(self.network.graph.nodes, indices)
         return nodes
 
-    def create_relational_lists(self, from_nodes, to_nodes, expand):
+    def _create_relational_lists(self, from_nodes, to_nodes, expand=True, sample=False):
         from_list = np.array([])
         if expand:
             from_list = from_nodes
         to_list = to_nodes
         for node in from_nodes:
             neighbors = self.network.graph.expand(node)
+            if sample:
+                length = len(neighbors) // 6
+                neighbors = neighbors[:length]
             new_connections = np.repeat(node, len(neighbors))
             from_list = np.concatenate((from_list, new_connections))
             neighbor_words = np.array([x[0] for x in list(neighbors)])
@@ -63,7 +75,14 @@ class RelationalGraph:
         return from_list, to_list
 
     def create_custom_relational(self, words):
-        from_list, to_list = self.create_relational_lists(words, np.array([]), False)
-        self._plot_nodes(from_list, to_list)
+        from_list, to_list = self._create_relational_lists(words, np.array([]), expand=False)
+        self._plot_nodes(from_list, to_list, [])
+
+    def create_colored_path(self, path):
+        #exception if empty path
+        from_nodes = np.array(path[:-1])
+        to_nodes = np.array(path[1:])
+        from_list, to_list = self._create_relational_lists(from_nodes, to_nodes, sample=True)
+        self._plot_nodes(from_list, to_list, path)
 
 
