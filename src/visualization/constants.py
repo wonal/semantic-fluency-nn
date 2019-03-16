59f7bcca877ed993625426235386b6e1319f2070
mkd <mkduer@gmail.com>
2019-03-15 21:15:19 -0700

    add simulated annealing and IRT constants

--- src/visualization/constants.py
+++ src/visualization/constants.py
@@ -1,19 +1,27 @@
 from os import getcwd        # directory operations
 
-# Plot Parameters
-RESTRICT_3D_TEXT = 50  # Restrict the number of annotations in 3D plot (computationally intensive)
-TOP_N = 100             # Total similar words grabbed for each key in the cluster graph
+# Word2Vec Model Hyperparameters
+W2V_ETA = 1  # TODO set to 20 when not testing
+
+# Simulated Annealing Algorithm Settings
+SA_ALPHA = 0.95
+SA_THRESH = 0.8
+SA_TEMP = 2000
+
+# IRT Plot Settings
+MAX_ITERATIONS = 7     # maximum number of iterations for algorithms during IRT comparisons
 
 # File/Directory Settings
 GRAPH_DIR = str(getcwd()) + '/images/'  # path and directory for saving graph images
-DELETE_GRAPHS = False                         # True clears all images, False leaves existing images
+DELETE_GRAPHS = False                   # True clears all images, False leaves existing images
+
+# TSNE Plot Settings
+RESTRICT_3D_TEXT = 125   # Restrict the number of annotations in 3D plot (computationally intensive)
+TOP_N = 100             # Total similar words grabbed for each key in the cluster graph
 
 # Corpus name and keys for cluster plot
 CLUSTER_KEYS = ['cat', 'sun', 'virtue', 'happy', 'sleeping', 'apple',
                 'poison', 'monster', 'wine', 'game', 'bird', 'heart']
-
-# Word2Vec Model Hyperparemeters
-W2V_ETA = 20
 
 # Cluster and 2D Embedding Hyperparameters
 C_PERPLEXITY = 5
@@ -28,6 +36,5 @@ DIM3_ITER = 2400
 DIM3_ETA = 400
 
 # Networkx Plot Coloring
-
 PATH_COLOR = 'green'
 NODE_COLOR = 'orange'
